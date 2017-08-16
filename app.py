922097fdac4b9e2f35e96b36827ba9e5ca1aa52f
Evamvid Sharma <evamvid@gmail.com>
2017-08-16 03:45:15 +0000

    added stdout flush

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask
 from flask import request
 from flask import render_template
 from flask import redirect
+import sys
 
 app = Flask(__name__)
 
@@ -18,9 +19,12 @@ def index():
 def parse_request(): 
     data = request.args.to_dict()
     print(data)
+    sys.stdout.flush()
     form = list(request.form.to_dict().keys())[0]
     print(form)
+    sys.stdout.flush()
     print(type(form))
+    sys.stdout.flush()
     barcodes.append(form)
     return str(form)
 

ec43d6d4e9a530af3f0e6f4534fc90aacd398d0f
Evamvid Sharma <evamvid@gmail.com>
2017-08-16 00:03:12 +0000

    Add files via upload

---
+++ barcodeserver.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+from flask import redirect
+
+app = Flask(__name__)
+
+barcodes = []
+
+@app.route('/', methods =['GET'])
+def index():
+    #return str(barcodes)	
+    return  render_template('index.html', barcodes = barcodes)
+
+@app.route('/', methods =['POST'])
+def parse_request(): 
+    data = request.args.to_dict()
+    print(data)
+    form = list(request.form.to_dict().keys())[0]
+    print(form)
+    print(type(form))
+    barcodes.append(form)
+    return str(form)
+
+@app.route('/result', methods =['POST', 'GET'])
+def clearall():
+    for item in barcodes:
+         barcodes.remove(item)
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0')
